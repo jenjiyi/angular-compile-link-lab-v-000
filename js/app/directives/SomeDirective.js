3e633b6fb1dbb85b23b3f0649995aac473d67cd2
JENNIFER YAMADA <jenny.yamada@gmail.com>
2016-10-05 10:06:08 -0700

    done

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -7,7 +7,19 @@ function SomeDirective() {
 		].join(''),
 		link: function (scope, elem, attrs) {
 
-		}
+		},
+		compile: function ($element) {
+			return {
+				pre: function (scope, element, attrs) {
+					element[0].textContent = "I love angular";
+				},
+				post: function (scope, element, attrs) {
+	        document.addEventListener("click", function(){
+						 alert("Hello!");
+					 });
+	      }
+			}
+    }
 	}
 }
 
